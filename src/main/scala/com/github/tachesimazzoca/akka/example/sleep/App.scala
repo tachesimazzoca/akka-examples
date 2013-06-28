ed1bf847a0779d0dd10294c2bbd63c5a05d08be1
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2013-06-28 20:12:31 +0900

    Rename case classes to App.*

--- src/main/scala/com/github/tachesimazzoca/akka/example/sleep/App.scala
+++ src/main/scala/com/github/tachesimazzoca/akka/example/sleep/App.scala
@@ -7,19 +7,21 @@ import akka.actor.Props
 class App extends Actor with ActorLogging {
   override def preStart(): Unit = {
     val sleeper = context.actorOf(Props[Sleeper])
-    sleeper ! (PrintRequest("Hello1 after 2sec"), 2000L)
-    sleeper ! (PrintRequest("Hello2 after 1sec"), 1000L)
+    sleeper ! (App.PrintRequest("Hello1 after 2sec"), 2000L)
+    sleeper ! (App.PrintRequest("Hello2 after 1sec"), 1000L)
     sleeper ! "Bad message"
     sleeper ! ("Bad message after 3sec", 3000L)
-    sleeper ! (ShutdownRequest, 5000L)
+    sleeper ! (App.ShutdownRequest, 5000L)
   }
 
   def receive = {
-    case PrintRequest(str: String) => println(str)
-    case ShutdownRequest => context.stop(self)
+    case App.PrintRequest(str: String) => println(str)
+    case App.ShutdownRequest => context.stop(self)
     case m @ _ => log.info("Unknown message - " + m)
   }
 }
 
-case class PrintRequest(str: String)
-case class ShutdownRequest()
+object App {
+  case class PrintRequest(str: String)
+  case class ShutdownRequest()
+}
